ae8c3c0ace894b8fde684a3e395fdd82d70367a8
praveen.reddy <p.reddy@kainos.com>
2021-09-23 16:29:27 +0100

    SFI-1033 : Added small changes

--- test/integration/narrow/routes/add-payment-hold.test.js
+++ test/integration/narrow/routes/add-payment-hold.test.js
@@ -1,14 +1,27 @@
-const { getResponse, postRequest } = require('../../../../app/payment-holds')
-const wreck = require('@hapi/wreck')
-describe('application task list route', () => {
-  jest.mock('../../../../app/payment-holds')
-  jest.mock('../../../../app/api')
+const cheerio = require('cheerio')
+const api = require('../../../../app/payment-holds')
 
+describe('application task list route', () => {
   let createServer
   let server
+  const paymentHoldCategories = [
+    {
+      holdCategoryId: 3,
+      name: 'DAX rejection',
+      schemeId: 1,
+      schemeName: 'SFI'
+    }
+  ]
+  const paymentHoldCategoriesResponse = {
+    payload: {
+      paymentHoldCategories
+    }
+  }
 
   beforeEach(async () => {
-    wreck.get = jest.fn()
+    jest.clearAllMocks()
+    api.getResponse = jest.fn()
+    api.postRequest = jest.fn()
     createServer = require('../../../../app/server')
     server = await createServer()
     await server.initialize()
@@ -18,23 +31,9 @@ describe('application task list route', () => {
     await server.stop()
   })
 
-  test('GET /add-payment-hold returns 200', async () => {
-    const wreckResponse = {
-      payload: null,
-      res: {
-        statusCode: 200
-      }
-    }
+  test('GET /add-payment-hold returns 200 & correctly lists returned hold category', async () => {
+    api.getResponse.mockReturnValue(paymentHoldCategoriesResponse)
 
-    wreck.get.mockReturnValue(wreckResponse)
-
-    jest.mock('@hapi/wreck')
-    jest.mock(
-      'getResponse',
-      () => {
-        return jest.fn(() => {})
-      }
-    )
     const options = {
       method: 'GET',
       url: '/add-payment-hold'
@@ -42,47 +41,9 @@ describe('application task list route', () => {
 
     const result = await server.inject(options)
     expect(result.statusCode).toBe(200)
+    const $ = cheerio.load(result.payload)
+    const holdCategories = $('.govuk-summary-list__value select option')
+    expect(holdCategories.length).toEqual(1)
+    expect(holdCategories.text()).toEqual(`${paymentHoldCategories[0].name} - ${paymentHoldCategories[0].schemeName}`)
   })
-
-  // test('GET /add-payment-hold returns application-task-list view', async () => {
-  //   const options = {
-  //     method: 'GET',
-  //     url: '/add-payment-hold'
-  //   }
-
-  //   const result = await server.inject(options)
-  //   expect(result.request.response.variety).toBe('view')
-  //   expect(result.request.response.source.template).toBe('application-task-list')
-  // })
 })
-
-// const schema = require('./schemas/frn')
-// const { getResponse, postRequest } = require('../payment-holds')
-
-// module.exports = [{
-//   method: 'GET',
-//   path: '/add-payment-hold',
-//   options: {
-//     handler: async (request, h) => {
-//       const paymentHoldCategoriesResponse = await getResponse('/payment-hold-categories')
-//       return h.view('add-payment-hold', { paymentHoldCategories: paymentHoldCategoriesResponse.payload.paymentHoldCategories })
-//     }
-//   }
-// },
-// {
-//   method: 'POST',
-//   path: '/add-payment-hold',
-//   options: {
-//     validate: {
-//       payload: schema,
-//       failAction: async (request, h, error) => {
-//         const paymentHoldCategoriesResponse = await getResponse('/payment-hold-categories')
-//         return h.view('add-payment-hold', { paymentHoldCategories: paymentHoldCategoriesResponse.payload.paymentHoldCategories, errors: error }).code(400).takeover()
-//       }
-//     },
-//     handler: async (request, h) => {
-//       await postRequest('/add-payment-hold', { holdCategoryId: request.payload.holdCategory, frn: request.payload.frn }, null)
-//       return h.redirect('/')
-//     }
-//   }
-// }]
