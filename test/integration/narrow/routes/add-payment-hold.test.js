68f8d7030fbe5e42da7ef317c9923f67c76247dc
fareed-mohammed <fareed.mohammed.uk@gmail.com>
2021-09-23 11:38:36 +0100

    unit test change

--- test/integration/narrow/routes/add-payment-hold.test.js
+++ test/integration/narrow/routes/add-payment-hold.test.js
@@ -1,14 +1,12 @@
-const { getResponse, postRequest } = require('../../../../app/payment-holds')
-const wreck = require('@hapi/wreck')
-describe('application task list route', () => {
-  jest.mock('../../../../app/payment-holds')
-  jest.mock('../../../../app/api')
+const api = require('../../../../app/payment-holds')
 
+describe('application task list route', () => {
   let createServer
   let server
 
   beforeEach(async () => {
-    wreck.get = jest.fn()
+    api.getResponse = jest.fn()
+    api.postRequest = jest.fn()
     createServer = require('../../../../app/server')
     server = await createServer()
     await server.initialize()
@@ -19,22 +17,14 @@ describe('application task list route', () => {
   })
 
   test('GET /add-payment-hold returns 200', async () => {
-    const wreckResponse = {
-      payload: null,
-      res: {
-        statusCode: 200
+    const paymentHoldCategoriesResponse = {
+      payload: {
+        paymentHoldCategories: {}
       }
     }
 
-    wreck.get.mockReturnValue(wreckResponse)
+    api.getResponse.mockReturnValue(paymentHoldCategoriesResponse)
 
-    jest.mock('@hapi/wreck')
-    jest.mock(
-      'getResponse',
-      () => {
-        return jest.fn(() => {})
-      }
-    )
     const options = {
       method: 'GET',
       url: '/add-payment-hold'
@@ -43,46 +33,4 @@ describe('application task list route', () => {
     const result = await server.inject(options)
     expect(result.statusCode).toBe(200)
   })
-
-  // test('GET /add-payment-hold returns application-task-list view', async () => {
-  //   const options = {
-  //     method: 'GET',
-  //     url: '/add-payment-hold'
-  //   }
-
-  //   const result = await server.inject(options)
-  //   expect(result.request.response.variety).toBe('view')
-  //   expect(result.request.response.source.template).toBe('application-task-list')
-  // })
 })
-
-// const schema = require('./schemas/frn')
-// const { getResponse, postRequest } = require('../payment-holds')
-
-// module.exports = [{
-//   method: 'GET',
-//   path: '/add-payment-hold',
-//   options: {
-//     handler: async (request, h) => {
-//       const paymentHoldCategoriesResponse = await getResponse('/payment-hold-categories')
-//       return h.view('add-payment-hold', { paymentHoldCategories: paymentHoldCategoriesResponse.payload.paymentHoldCategories })
-//     }
-//   }
-// },
-// {
-//   method: 'POST',
-//   path: '/add-payment-hold',
-//   options: {
-//     validate: {
-//       payload: schema,
-//       failAction: async (request, h, error) => {
-//         const paymentHoldCategoriesResponse = await getResponse('/payment-hold-categories')
-//         return h.view('add-payment-hold', { paymentHoldCategories: paymentHoldCategoriesResponse.payload.paymentHoldCategories, errors: error }).code(400).takeover()
-//       }
-//     },
-//     handler: async (request, h) => {
-//       await postRequest('/add-payment-hold', { holdCategoryId: request.payload.holdCategory, frn: request.payload.frn }, null)
-//       return h.redirect('/')
-//     }
-//   }
-// }]
