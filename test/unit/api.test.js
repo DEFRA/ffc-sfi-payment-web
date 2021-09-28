6bc2a97c8bcce82ba9fe6b4d4b74619a9d09e4c1
Steven Hunt <st3v3nhunt@users.noreply.github.com>
2021-09-28 09:54:29 +0100

    feat: add unit tests for payments api

---
+++ test/unit/api.test.js
@@ -0,0 +1,48 @@
+const { paymentsEndpoint } = require('../../app/config')
+
+describe('API', () => {
+  const api = require('../../app/api')
+  const wreck = require('@hapi/wreck')
+  jest.mock('@hapi/wreck')
+
+  const url = 'domain.com'
+
+  beforeEach(() => {
+    jest.clearAllMocks()
+  })
+
+  test.each([
+    { token: 'token', expectedAuthVal: 'token' },
+    { token: null, expectedAuthVal: '' },
+    { token: undefined, expectedAuthVal: '' }
+  ])('Get makes request for JSON with auth header and returns response', async ({ token, expectedAuthVal }) => {
+    const responseMock = { payload: 'something' }
+    wreck.get.mockResolvedValueOnce(responseMock)
+
+    const response = await api.get(url, token)
+
+    expect(wreck.get).toHaveBeenCalledTimes(1)
+    expect(wreck.get).toHaveBeenCalledWith(`${paymentsEndpoint}${url}`, { headers: { Authorization: expectedAuthVal }, json: true })
+    expect(response).toEqual(responseMock)
+  })
+
+  test.each([
+    { token: 'token', expectedAuthVal: 'token' },
+    { token: null, expectedAuthVal: '' },
+    { token: undefined, expectedAuthVal: '' }
+  ])('POST makes request for JSON with auth header and returns payload from response', async ({ token, expectedAuthVal }) => {
+    const responseMock = { payload: 'something' }
+    wreck.post.mockResolvedValueOnce(responseMock)
+    const data = { hi: 'world' }
+
+    const response = await api.post(url, data, token)
+
+    expect(wreck.post).toHaveBeenCalledTimes(1)
+    expect(wreck.post).toHaveBeenCalledWith(`${paymentsEndpoint}${url}`, {
+      headers: { Authorization: expectedAuthVal },
+      json: true,
+      payload: data
+    })
+    expect(response).toEqual(responseMock.payload)
+  })
+})
