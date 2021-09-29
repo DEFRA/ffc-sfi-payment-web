0d9b52549adb0983c7305ea4969b4f9747320aad
Steven Hunt <st3v3nhunt@users.noreply.github.com>
2021-09-29 09:59:42 +0100

    feat: add unit tests for GETting and POSTing payment holds

---
+++ test/unit/payment-holds.test.js
@@ -0,0 +1,70 @@
+
+describe('Payment holds', () => {
+  const paymentHolds = require('../../app/payment-holds')
+  jest.mock('../../app/api')
+  const api = require('../../app/api')
+
+  const errorMessage = 'something has gone wrong'
+  const url = 'https://a.url.com'
+
+  console.error = jest.fn()
+
+  beforeEach(() => {
+    jest.clearAllMocks()
+  })
+
+  describe('getResponse', () => {
+    test('returns response when no error', async () => {
+      const mockResponse = 'something'
+      api.get.mockResolvedValueOnce(mockResponse)
+
+      const response = await paymentHolds.getResponse(url)
+
+      expect(response).toEqual(mockResponse)
+      expect(api.get).toHaveBeenCalledTimes(1)
+      expect(api.get).toHaveBeenCalledWith(url)
+    })
+
+    test('returns undefined when error occurrs', async () => {
+      const error = new Error(errorMessage)
+      api.get.mockRejectedValueOnce(error)
+
+      const response = await paymentHolds.getResponse(url)
+
+      expect(response).toEqual(undefined)
+      expect(api.get).toHaveBeenCalledTimes(1)
+      expect(api.get).toHaveBeenCalledWith(url)
+      expect(console.error).toHaveBeenCalledTimes(1)
+      expect(console.error).toHaveBeenCalledWith(error)
+    })
+  })
+
+  describe('postRequest', () => {
+    const data = 123
+    const token = 'token'
+
+    test('returns response when no error', async () => {
+      const mockResponse = 'something'
+      api.post.mockResolvedValueOnce(mockResponse)
+
+      const response = await paymentHolds.postRequest(url, data, token)
+
+      expect(response).toEqual(mockResponse)
+      expect(api.post).toHaveBeenCalledTimes(1)
+      expect(api.post).toHaveBeenCalledWith(url, data, token)
+    })
+
+    test('returns undefined when error occurrs', async () => {
+      const error = new Error(errorMessage)
+      api.post.mockRejectedValueOnce(error)
+
+      const response = await paymentHolds.postRequest(url, data, token)
+
+      expect(response).toEqual(undefined)
+      expect(api.post).toHaveBeenCalledTimes(1)
+      expect(api.post).toHaveBeenCalledWith(url, data, token)
+      expect(console.error).toHaveBeenCalledTimes(1)
+      expect(console.error).toHaveBeenCalledWith(error)
+    })
+  })
+})
