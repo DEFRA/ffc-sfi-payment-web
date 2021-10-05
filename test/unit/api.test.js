5dbec5037e59e96d7bfdebc45cb28c6fa1d5a65c
Steven Hunt <st3v3nhunt@users.noreply.github.com>
2021-10-05 11:52:01 +0100

    refactor: rewrite test to use Nock

--- test/unit/api.test.js
+++ test/unit/api.test.js
@@ -1,48 +1,54 @@
+const nock = require('nock')
+const api = require('../../app/api')
 const { paymentsEndpoint } = require('../../app/config')
 
 describe('API', () => {
-  const api = require('../../app/api')
-  const wreck = require('@hapi/wreck')
-  jest.mock('@hapi/wreck')
-
-  const url = 'domain.com'
+  const url = '/request-path'
 
   beforeEach(() => {
     jest.clearAllMocks()
   })
 
-  test.each([
-    { token: 'token', expectedAuthVal: 'token' },
-    { token: null, expectedAuthVal: '' },
-    { token: undefined, expectedAuthVal: '' }
-  ])('Get makes request for JSON with auth header and returns response', async ({ token, expectedAuthVal }) => {
-    const responseMock = { payload: 'something' }
-    wreck.get.mockResolvedValueOnce(responseMock)
+  afterAll(() => {
+    nock.cleanAll()
+  })
+
+  const testCases = [
+    { token: 'token', Authorization: 'token' },
+    { token: null, Authorization: '' },
+    { token: undefined, Authorization: '' }
+  ]
+  const responseMockData = { numbers: [0, 1, 2] }
+
+  test.each(testCases)('Get makes GET request with auth header and returns response', async ({ token, Authorization }) => {
+    const scope = nock(paymentsEndpoint, {
+      reqheaders: {
+        Authorization
+      }
+    })
+      .get(url)
+      .reply(200, responseMockData)
 
     const response = await api.get(url, token)
 
-    expect(wreck.get).toHaveBeenCalledTimes(1)
-    expect(wreck.get).toHaveBeenCalledWith(`${paymentsEndpoint}${url}`, { headers: { Authorization: expectedAuthVal }, json: true })
-    expect(response).toEqual(responseMock)
+    expect(response).toHaveProperty('payload')
+    expect(response.payload).toEqual(responseMockData)
+    expect(scope.isDone()).toEqual(true)
   })
 
-  test.each([
-    { token: 'token', expectedAuthVal: 'token' },
-    { token: null, expectedAuthVal: '' },
-    { token: undefined, expectedAuthVal: '' }
-  ])('POST makes request for JSON with auth header and returns payload from response', async ({ token, expectedAuthVal }) => {
-    const responseMock = { payload: 'something' }
-    wreck.post.mockResolvedValueOnce(responseMock)
+  test.each(testCases)('Post makes POST request with data and auth header and returns payload from response', async ({ token, Authorization }) => {
     const data = { hi: 'world' }
+    const scope = nock(paymentsEndpoint, {
+      reqheaders: {
+        Authorization
+      }
+    })
+      .post(url, data)
+      .reply(200, responseMockData)
 
     const response = await api.post(url, data, token)
 
-    expect(wreck.post).toHaveBeenCalledTimes(1)
-    expect(wreck.post).toHaveBeenCalledWith(`${paymentsEndpoint}${url}`, {
-      headers: { Authorization: expectedAuthVal },
-      json: true,
-      payload: data
-    })
-    expect(response).toEqual(responseMock.payload)
+    expect(response).toEqual(responseMockData)
+    expect(scope.isDone()).toEqual(true)
   })
 })
