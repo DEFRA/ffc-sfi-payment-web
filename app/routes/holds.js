0b0819b6988034117176dc218fb10a053318b0b0
John Watson <johnwatson484@hotmail.com>
2022-02-24 20:58:59 +0000

    Update holds

--- app/routes/holds.js
+++ app/routes/holds.js
@@ -36,15 +36,6 @@ module.exports = [{
       return h.redirect('/payment-holds')
     }
   }
-}, {
-  method: 'GET',
-  path: '/remove-payment-hold',
-  options: {
-    handler: async (request, h) => {
-      const paymentHoldsResponse = await get('/payment-holds?open=true')
-      return h.view('remove-payment-hold', { paymentHolds: paymentHoldsResponse.payload.paymentHolds })
-    }
-  }
 },
 {
   method: 'POST',
