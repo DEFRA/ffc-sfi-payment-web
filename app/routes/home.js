7609a83ad9abc47ff01920f58cfdff07807d3bd6
John Watson <johnwatson484@hotmail.com>
2022-03-10 09:23:47 +0000

    Add home page totals

--- app/routes/home.js
+++ app/routes/home.js
@@ -1,11 +1,15 @@
+const { get } = require('../api')
+
 module.exports = {
   method: 'GET',
   path: '/',
   options: {
     handler: async (request, h) => {
+      const paymentHoldsResponse = await get('/payment-holds')
+      const schemes = await get('/payment-schemes')
       return h.view('home', {
-        totalHolds: 0,
-        totalSchemes: 0
+        totalHolds: paymentHoldsResponse.payload.paymentHolds?.length ?? 0,
+        totalSchemes: schemes.payload.paymentSchemes?.length ?? 0
       })
     }
   }
