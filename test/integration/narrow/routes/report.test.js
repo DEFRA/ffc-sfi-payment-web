667525027aeb34658107dd6609cca7b6cfa964c0
John Watson <johnwatson484@hotmail.com>
2022-04-20 17:39:00 +0100

    Add tests

--- test/integration/narrow/routes/report.test.js
+++ test/integration/narrow/routes/report.test.js
@@ -1,5 +1,26 @@
 const { schemeAdmin } = require('../../../../app/auth/permissions')
+let mockDownload
 jest.mock('../../../../app/auth')
+jest.mock('@azure/storage-blob', () => {
+  return {
+    BlobServiceClient: {
+      fromConnectionString: jest.fn().mockImplementation(() => {
+        return {
+          getContainerClient: jest.fn().mockImplementation(() => {
+            return {
+              createIfNotExists: jest.fn(),
+              getBlockBlobClient: jest.fn().mockImplementation(() => {
+                return {
+                  download: mockDownload
+                }
+              })
+            }
+          })
+        }
+      })
+    }
+  }
+})
 
 describe('Report test', () => {
   const createServer = require('../../../../app/server')
@@ -7,12 +28,15 @@ describe('Report test', () => {
   let auth
 
   beforeEach(async () => {
+    mockDownload = jest.fn().mockReturnValue({
+      readableStreamBody: 'Hello'
+    })
     auth = { strategy: 'session-auth', credentials: { scope: [schemeAdmin] } }
     server = await createServer()
     await server.initialize()
   })
 
-  test('GET /report route returns 200', async () => {
+  test('GET /report route returns stream if report available', async () => {
     const options = {
       method: 'GET',
       url: '/report',
@@ -21,6 +45,25 @@ describe('Report test', () => {
 
     const response = await server.inject(options)
     expect(response.statusCode).toBe(200)
+    expect(response.payload).toBe('Hello')
+  })
+
+  afterEach(async () => {
+    await server.stop()
+    jest.resetAllMocks()
+  })
+
+  test('GET /report route returns unavailable page if report not available', async () => {
+    mockDownload = jest.fn().mockReturnValue(undefined)
+    const options = {
+      method: 'GET',
+      url: '/report',
+      auth
+    }
+
+    const response = await server.inject(options)
+    console.log(response)
+    expect(response.payload).toContain('Payment report unavailable')
   })
 
   afterEach(async () => {
