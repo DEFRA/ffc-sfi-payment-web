dd94a1309c16c66ca64c27c96f907dade15b2821
John Watson <42574977+johnwatson484@users.noreply.github.com>
2022-06-07 13:40:12 +0100

    Fix hold date format (#38)

--- app/format-date.js
+++ app/format-date.js
@@ -1,6 +1,6 @@
 const moment = require('moment')
 
-const formatDate = (dateToFormat, currentDateFormat = 'YYYY-MM-DD') => {
+const formatDate = (dateToFormat, currentDateFormat = 'DD/MM/YYYY HH:mm') => {
   if (dateToFormat) {
     return moment(dateToFormat, currentDateFormat).format('DD/MM/YYYY')
   }
