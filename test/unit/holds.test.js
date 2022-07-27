15cdb6d260f74cbc98c35ab1960ddf356b642828
Simon Dunn <simondunn90@gmail.com>
2022-07-27 09:12:56 +0100

    Add Vet Visit assurance check hold (#40)
    * change schemeName for Vet Visits

---
+++ test/unit/holds.test.js
@@ -0,0 +1,52 @@
+const { getHoldCategories } = require('../../app/holds')
+jest.mock('../../app/api.js')
+const { get } = require('../../app/api')
+
+describe('Get holds categories', () => {
+  const mockPaymentHoldCategories = [{
+    holdCategoryId: 123,
+    name: 'my hold category',
+    schemeName: 'Scheme Name'
+  }]
+
+  const mockGetPaymentHoldCategories = (paymentHoldCategories) => {
+    get.mockResolvedValue({ payload: { paymentHoldCategories } })
+  }
+
+  test('Should return a schemeName of "Annual Health and Welfare Review" when schemeName is "Vet Visits"', async () => {
+    mockPaymentHoldCategories[0].schemeName = 'Vet Visits'
+    mockGetPaymentHoldCategories(mockPaymentHoldCategories)
+
+    const result = await getHoldCategories(mockGetPaymentHoldCategories)
+
+    expect(result.schemes[0]).toBe('Annual Health and Welfare Review')
+  })
+
+  test('Should return a schemeName of "SFI" when schemeName is "SFI"', async () => {
+    mockPaymentHoldCategories[0].schemeName = 'SFI'
+    mockGetPaymentHoldCategories(mockPaymentHoldCategories)
+    const result = await getHoldCategories(mockGetPaymentHoldCategories)
+    expect(result.schemes[0]).toBe('SFI')
+  })
+
+  test('Should return a schemeName of "SFI Pilot" when schemeName is "SFI Pilot"', async () => {
+    mockPaymentHoldCategories[0].schemeName = 'SFI Pilot'
+    mockGetPaymentHoldCategories(mockPaymentHoldCategories)
+    const result = await getHoldCategories(mockGetPaymentHoldCategories)
+    expect(result.schemes[0]).toBe('SFI Pilot')
+  })
+
+  test('Should return a schemeName of "Lump Sums" when schemeName is "Lump Sums"', async () => {
+    mockPaymentHoldCategories[0].schemeName = 'Lump Sums'
+    mockGetPaymentHoldCategories(mockPaymentHoldCategories)
+    const result = await getHoldCategories(mockGetPaymentHoldCategories)
+    expect(result.schemes[0]).toBe('Lump Sums')
+  })
+
+  test('Should return a schemeName of "LNR" when schemeName is "LNR"', async () => {
+    mockPaymentHoldCategories[0].schemeName = 'LNR'
+    mockGetPaymentHoldCategories(mockPaymentHoldCategories)
+    const result = await getHoldCategories(mockGetPaymentHoldCategories)
+    expect(result.schemes[0]).toBe('LNR')
+  })
+})
