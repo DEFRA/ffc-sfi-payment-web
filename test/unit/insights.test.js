ff7b49e60b4a88c87cfe1d24ad31214f723941a7
Marc Templeton <marcte@kainos.com>
2023-02-17 09:49:00 +0000

    jest -- I WOOONNN

--- test/unit/insights.test.js
+++ test/unit/insights.test.js
@@ -1,16 +1,19 @@
-const mockApplicationInsights = require('../mocks/objects/application-insights')
-
-const insights = require('../../app/insights')
-
 const applicationName = require('../mocks/components/application-name')
+
+let mockApplicationInsights
+let insights
 
 describe('App Insight setup', () => {
   beforeEach(() => {
     process.env.APPINSIGHTS_CLOUDROLE = applicationName
+
+    mockApplicationInsights = require('../mocks/objects/application-insights')
+    insights = require('../../app/insights')
   })
 
   afterEach(() => {
     jest.clearAllMocks()
+    jest.resetModules()
   })
 
   describe('When process.env.APPINSIGHTS_CONNECTIONSTRING exists', () => {
@@ -62,6 +65,11 @@ describe('App Insight setup', () => {
     test('should not call mockApplicationInsights.setup.start', () => {
       insights.setup()
       expect(mockApplicationInsights.setup().start).not.toHaveBeenCalled()
+    })
+
+    test('should have undefined as value for mockApplicationInsights.defaultClient.context.tags[mockApplicationInsights.defaultClient.context.keys.cloudRole] key', () => {
+      insights.setup()
+      expect(mockApplicationInsights.defaultClient.context.tags[mockApplicationInsights.defaultClient.context.keys.cloudRole]).toBeUndefined()
     })
   })
 })
