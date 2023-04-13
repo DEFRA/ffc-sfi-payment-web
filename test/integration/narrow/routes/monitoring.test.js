f0565a3dd10fe8e8d4a1b084542fd84eb5e365a1
John Watson <john.watson2@rpa.gov.uk>
2023-04-13 17:56:22 +0100

    Add tests

---
+++ test/integration/narrow/routes/monitoring.test.js
@@ -0,0 +1,60 @@
+jest.mock('../../../../app/auth')
+
+jest.mock('../../../../app/config')
+const config = require('../../../../app/config')
+
+const createServer = require('../../../../app/server')
+
+const { holdAdmin } = require('../../../../app/auth/permissions')
+
+let server
+let auth
+
+describe('monitoring test', () => {
+  beforeEach(async () => {
+    config.useV1Events = true
+    config.useV2Events = true
+    auth = { strategy: 'session-auth', credentials: { scope: [holdAdmin] } }
+    server = await createServer()
+    await server.initialize()
+  })
+
+  afterEach(async () => {
+    await server.stop()
+  })
+
+  test('GET /monitoring route returns 200 if V2 events enabled', async () => {
+    const options = {
+      method: 'GET',
+      url: '/monitoring',
+      auth
+    }
+
+    const response = await server.inject(options)
+    expect(response.statusCode).toBe(200)
+  })
+
+  test('GET /monitoring route returns monitoring view if V2 events enabled', async () => {
+    const options = {
+      method: 'GET',
+      url: '/monitoring',
+      auth
+    }
+
+    const response = await server.inject(options)
+    expect(response.payload).toContain('Monitoring')
+  })
+
+  test('GET /monitoring route redirects to event projection route if V2 events disabled', async () => {
+    config.useV2Events = false
+    const options = {
+      method: 'GET',
+      url: '/monitoring',
+      auth
+    }
+
+    const response = await server.inject(options)
+    expect(response.statusCode).toBe(302)
+    expect(response.headers.location).toBe('/event-projection')
+  })
+})
