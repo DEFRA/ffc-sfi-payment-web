0356ce71c632132ce40c56d16979ef42e836c2e3
JungaBunga <amir.ishmuhametov@gmail.com>
2024-04-17 17:54:20 +0100

    changed the way report-types are exported to provide higher flexibility

--- app/constants/report-types.js
+++ app/constants/report-types.js
@@ -1,8 +1,10 @@
-module.exports.getReportTypes = () => {
-    return {
-        'Payment request statuses': 'payment-requests',
-        'Combined transaction report': 'transaction-summary',
-        'Suppressed payment requests': 'suppressed-payments',
-        'AP Listing Report': 'ap-listing'
-    }
+const getReportTypes = () => ({
+    'Payment request statuses': 'payment-requests',
+    'Combined transaction report': 'transaction-summary',
+    'Suppressed payment requests': 'suppressed-payments',
+    'AP Listing Report': 'ap-listing'
+})
+
+module.exports = {
+    getReportTypes 
 }
