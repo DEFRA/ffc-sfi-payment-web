702505ecb2073504db1dbccbc97c843dabbe977e
Sam Plackett <samuel.plackett@eviden.com>
2024-05-10 14:28:08 +0100

    bump version

--- app/constants/report-types.js
+++ app/constants/report-types.js
@@ -2,8 +2,9 @@ const getReportTypes = () => ({
   'Payment request statuses': 'payment-requests',
   'Combined transaction report': 'transaction-summary',
   'Suppressed payment requests': 'suppressed-payments',
-  'AP-AR Listing Report': 'ap-ar-listing',
-  Holds: 'holds'
+  'AP-AR listing report': 'ap-ar-listing',
+  Holds: 'holds',
+  'Request Editor report': 'request-editor'
 })
 
 module.exports = {
